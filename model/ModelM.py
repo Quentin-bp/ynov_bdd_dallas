2379010e04bbe56378a26d2941b94b10c6d95b30
fereol gbenou <gbenoufereol@yahoo.com>
2023-12-07 12:19:07 +0100

    initial config repo

--- model/ModelM.py
+++ model/ModelM.py
@@ -1,7 +1,6 @@
 
 class Model:
     def __init__(self):
-<<<<<<< HEAD
         self.__id: int = None
 
     def setID(self, id: int) -> None:
@@ -9,12 +8,3 @@ class Model:
 
     def getID(self) -> int:
         return self.__id
-=======
-        self.id: int = None
-
-    def setID(self, _id: int) -> None:
-        self.id = _id
-
-    def getID(self) -> int:
-        return self.id
->>>>>>> 68d1df8aeaf75441f47f38b44bddd9deb6605ba2
--- model/TownsM.py
+++ model/TownsM.py
@@ -3,7 +3,6 @@ from ModelM import Model
 class Towns(Model):
 
     def __init__(self):
-<<<<<<< HEAD
         self.__name: str = ""
         self.__adresse_code: str = ""
         
@@ -18,19 +17,3 @@ class Towns(Model):
 
     def getAddressCode(self) -> str:
         return self.__adresse_code
-=======
-        self.name: str = ""
-        self.address_code: str = ""
-        
-    def setName(self, _name: str) -> None:
-        self.name = _name
-
-    def getName(self) -> str:
-        return self.name
-
-    def setAddressCode(self, _address_code: str) -> None:
-        self.address_code = _address_code
-
-    def getAddressCode(self) -> str:
-        return self.address_code
->>>>>>> 68d1df8aeaf75441f47f38b44bddd9deb6605ba2
